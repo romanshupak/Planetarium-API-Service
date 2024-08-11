c4f0de725402545531330438495e133e15d57fc2
Roman Shupak <romanshupak808@gmail.com>
2024-08-11 17:33:18 +0300

    fill up .dockerignore additional settings, add new line in .env.sample, remove redundant comments in tests_show_session_api

--- planetarium/tests/tests_show_session_api.py
+++ planetarium/tests/tests_show_session_api.py
@@ -1,64 +1,3 @@
-# from django.contrib.auth import get_user_model
-# from django.test import TestCase
-# from rest_framework import status
-#
-# from rest_framework.reverse import reverse
-# from rest_framework.test import APIClient
-#
-# from planetarium.models import ShowSession
-# from planetarium.serializers import ShowSessionSerializer
-#
-# SHOW_SESSION_URL = reverse("planetarium:showsession-list")
-#
-#
-# def sample_user(is_staff=False, **params):
-#     """Create and return a sample user"""
-#     return get_user_model().objects.create_user(**params, is_staff=is_staff)
-#
-#
-# def sample_show_session(**params):
-#     defaults = {
-#         "astronomy_show": "test show",
-#         "planetarium_dome": "test dome",
-#         "show_time": "2024-12-06"
-#     }
-#     defaults.update(params)
-#     return ShowSession.objects.create(**defaults)
-#
-#
-# class PublicShowSessionTests(TestCase):
-#     """Test unauthenticated API requests"""
-#
-#     def setUp(self):
-#         self.client = APIClient()
-#
-#     def test_auth_required(self):
-#         """Test that authentication is required"""
-#         res = self.client.get(SHOW_SESSION_URL)
-#         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-#
-#
-# class PrivateShowSessionTests(TestCase):
-#     """Test authenticated API requests"""
-#
-#     def setUp(self):
-#         self.client = APIClient()
-#         self.user = sample_user(email="test@test.com", password="testpass")
-#         self.client.force_authenticate(self.user)
-#
-#     def test_retrieve_showsessions(self):
-#         """Test retrieving a list of ShowSessions"""
-#         sample_show_session()
-#         sample_show_session(name="Another Session")
-#
-#         res = self.client.get(SHOW_SESSION_URL)
-#
-#         show_sessions = ShowSession.objects.all()
-#         serializer = ShowSessionSerializer(show_sessions, many=True)
-#
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#         self.assertEqual(res.data, serializer.data)
-
 from datetime import datetime
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -67,7 +6,7 @@ from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
 from planetarium.models import ShowSession, AstronomyShow, PlanetariumDome
-from planetarium.serializers import ShowSessionSerializer, ShowSessionListSerializer
+from planetarium.serializers import ShowSessionListSerializer
 
 SHOW_SESSION_URL = reverse("planetarium:showsession-list")
 
